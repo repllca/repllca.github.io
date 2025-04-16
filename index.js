64b716832a8ad908dfa22e0a9ac46a0580558664
repllca <maridann1022@icloud.com>
2025-04-17 04:57:34 +0900

    add javas

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function ShowAlert() {
-    alert("Hello world!!");
-  }
+function addMenu(category, item) {
+  const menuList = document.getElementById("menuList");
+  const listItem = document.createElement("li");
+  listItem.textContent = `[${category}] ${item}`;
+  menuList.appendChild(listItem);
+}
+
+function resetMenu() {
+  const menuList = document.getElementById("menuList");
+  menuList.innerHTML = "";
+}
